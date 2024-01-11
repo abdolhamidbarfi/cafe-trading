4dc1ac59a8f4b7f65f5aa3d571ed8f3490bc4811
abdolhamidbarfi <abdolhamidbarfi@yahoo.com>
2024-01-12 01:06:39 +0330

    Initial launch of the project

---
+++ src/app/slice-simulator/page.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import { SliceSimulator } from "@slicemachine/adapter-next/simulator";
+import { SliceZone } from "@prismicio/react";
+
+// import { components } from "../../slices";
+
+export default function SliceSimulatorPage() {
+  return (
+    <h1>راه اندازی اولیه</h1>
+    // <SliceSimulator
+    //   sliceZone={(props) => <SliceZone {...props} components={components} />}
+    // />
+  );
+}
